91ce0767f0f03491dba2b1ecc97d0e6b039efcd5
Lukas Wurzinger <lukas@wrz.one>
2024-08-19 23:01:06 +0200

    helix

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,10 @@ if status is-interactive
   set fish_cursor_replace_one underscore blink
   set fish_cursor_visual      block
 
+  if type -q direnv
+    direnv hook fish | source
+  end
+
   abbr --add l ls
   abbr --add lsa ls -a
   abbr --add la ls -a
