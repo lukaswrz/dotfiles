5a0a85aed973c1fe71b6c31edfa3d5ff14dc2621
Lukas Wurzinger <lukas@wrz.one>
2024-12-29 22:20:56 +0100

    stuff

--- flake.nix
+++ flake.nix
@@ -3,7 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    devenv.url = "github:cachix/devenv";
   };
 
   outputs = {
@@ -12,6 +19,10 @@
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
       systems = nixpkgs.lib.systems.flakeExposed;
 
       perSystem = {
@@ -21,10 +32,21 @@
       }: {
         packages.plow = pkgs.callPackage ./plow {};
 
-        devShells.default = pkgs.mkShellNoCC {
-          PLOW_FROM = "./home";
+        devenv.shells.default = {
+          devenv.root = let
+            devenvRootFileContent = builtins.readFile inputs.devenv-root.outPath;
+          in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
 
-          packages = [self'.packages.plow];
+          env.PLOW_FROM = "./home";
+
+          imports = [
+            ./devenv.nix
+          ];
+
+          packages = [
+            self'.packages.plow
+          ];
         };
       };
     };
--- home/bash/.bashrc
+++ home/bash/.bashrc
@@ -1,1 +1,8 @@
-source /etc/bash.bashrc
+# shellcheck shell=bash
+
+for rc in /etc/bashrc /etc/*.bashrc; do
+  if [[ -e $rc ]]; then
+    # shellcheck disable=SC1090
+    source "$rc"
+  fi
+done
