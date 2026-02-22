cce09181d71305cf945263bd79b74e2674f68335
Lukas Wurzinger <lukas@wrz.one>
2026-02-22 13:27:28 +0100

    fix

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -84,18 +84,18 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     set_color $color_cwd
-    echo -n -- (prompt_pwd)
+    echo -n -- "$(prompt_pwd) "
     set_color normal
 
     echo -n -- (fish_vcs_prompt)' '
 
     set -l status_color (set_color $fish_color_status)
     set -l statusb_color (set_color --bold $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    set -l prompt_status (__fish_print_pipestatus '[' ']' '|' "$status_color" "$statusb_color" $last_pipestatus)
     echo -n $prompt_status
     set_color normal
 
-    echo -n -- $suffix ''
+    echo -n -- "$suffix "
 end
 
 fish_add_path ~/.local/bin
