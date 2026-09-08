4036d2de94a83de151051081a026d4de6f27f461
Lukas Wurzinger <lukas@wrz.one>
2026-09-08 20:09:22 +0200

    vis

---
+++ home/vis/.config/vis/visrc.lua
@@ -0,0 +1,18 @@
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+    vis:command('set theme base16-darktooth')
+
+    vis:command('map! normal j gj')
+    vis:command('map! normal k gk')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+    vis:command('set autoindent on')
+    vis:command('set colorcolumn 80')
+    vis:command('set cursorline')
+    vis:command('set number')
+    vis:command('set relativenumbers')
+    vis:command('set show-spaces off')
+    vis:command('set show-tabs on')
+end)
